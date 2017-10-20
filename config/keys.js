2a4cdf6427fd76f3c259bb6ff23eff974b59c441
KaiKeanaaina <kaikeanaaina@gmail.com>
2017-10-19 15:49:09 -1000

    testing

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,16 @@
 // keys.js - figure out what set of credentials to return
-if (process.env.NODE_ENV === 'production') {
-  // we are in production - return the prod set of keys
-  module.exports = require('./prod')
-} else {
-  // we are in development - return the dev keys!!!
-  module.exports = require('./dev')
+// if (process.env.NODE_ENV === 'production') {
+//   // we are in production - return the prod set of keys
+//   module.exports = require('./prod')
+// } else {
+//   // we are in development - return the dev keys!!!
+//   module.exports = require('./dev')
+// }
+
+module.exports = {
+  googleClientID:
+    '904303509470-m74v01njnfe4cr0utqo5hgoh6gd04ov6.apps.googleusercontent.com',
+  googleClientSecret: '1mEMlFca_7NhJls3e_POSgPY',
+  mongoURI: 'mongodb://kainoa:password@ds019956.mlab.com:19956/emaily-dev',
+  cookieKey: ';alskdfj;alsdkjf;asdlkfasd;fjkasd;lkfj;kljkfdjghs;dflkgj',
 }
